a67400682e47513250d76a3dec1aae29cb1539d2
nguyenxuanthanh0807 <nguyenxuanthanh0807@gmail.com>
2024-01-30 20:11:51 +0700

    Complete Update Infor

--- lib/app/routes/api.routes.dart
+++ lib/app/routes/api.routes.dart
@@ -1,5 +1,5 @@
 class ApiRoutes {
-  static const String baseurl = "https://92cd-118-71-126-41.ngrok-free.app";
+  static const String baseurl = "https://554a-115-75-223-123.ngrok-free.app";
 }
 
 String domain = ApiRoutes.baseurl;
